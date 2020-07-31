4d6e256e0e59b5658192aa77c06cdd67500ce12a
avigehlot05@gmail.com <avigehlot05@gmail.com>
2020-07-31 21:40:32 +0530

    a

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const config = require("./config/key");
+require('dotenv').config();
 
 
 
