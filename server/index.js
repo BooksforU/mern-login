2f4d6585f6b6bcb0c2f1b5dba4123e791107e0a9
avigehlot05@gmail.com <avigehlot05@gmail.com>
2020-07-31 22:38:59 +0530

    ok

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const config = require('./config/key')
 
-
+// 
 
 const mongoose = require("mongoose");
 mongoose.connect(
